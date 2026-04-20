fe31e55424ea5f74e3bdce648d6d7db9305ee174
Valera35981 <valerkamagakyan@gmail.com>
2026-04-20 15:01:58 +0300

    Add files via upload

---
+++ Лаб2_зад2_прв/Лаб2_зад2_прв/Лаб2_зад2_прв.cpp
@@ -0,0 +1,231 @@
+﻿#include <iostream>
+#include <thread>
+#include <vector>
+#include <queue>
+#include <mutex>
+#include <semaphore>
+#include <chrono>
+#include <random>
+#include <atomic>
+#include <condition_variable>
+
+using namespace std;
+using namespace chrono;
+
+struct DataPacket {
+    int id;                 
+    int priority;           
+    int size;               
+    bool isCritical;        
+    int stationId;          
+    bool operator<(const DataPacket& other) const {
+        if (isCritical != other.isCritical)
+            return !isCritical;
+        return priority > other.priority;
+    }
+};
+const int INITIAL_SERVERS = 3;           
+const int MAX_SERVERS = 6;               
+const int TOTAL_STATIONS = 10;           
+const int PACKETS_PER_STATION = 5;       
+const double LOAD_THRESHOLD = 0.8;       
+
+atomic<int> activeServers(INITIAL_SERVERS);     
+atomic<int> totalActivePackets(0);             
+atomic<int> packetCounter(0);                   
+atomic<int> packetsSent(0);                     
+atomic<int> packetsProcessed(0);                
+atomic<bool> systemRunning(true);               
+atomic<bool> emergencyMode(false);              
+
+counting_semaphore<MAX_SERVERS> serverSem(INITIAL_SERVERS);
+mutex consoleMutex;        
+mutex queueMutex;          
+priority_queue<DataPacket> packetQueue; 
+
+random_device rd;
+mt19937 gen(rd());
+uniform_int_distribution<> priorityDist(1, 10);      
+uniform_int_distribution<> sizeDist(10, 100);        
+uniform_int_distribution<> criticalChance(1, 10);    
+void processPacket(DataPacket packet) {
+    serverSem.acquire();
+    totalActivePackets++;
+    {
+        lock_guard<mutex> lock(consoleMutex);
+        cout << "[СЕРВЕР] Обрабатывает пакет #" << packet.id
+            << " | приоритет: " << packet.priority
+            << (packet.isCritical ? " | КРИТИЧЕСКИЙ" : "")
+            << " | станция: " << packet.stationId
+            << " | размер: " << packet.size << " усл.ед." << endl;
+    }
+    int processTime = packet.size / 20 + 1; 
+    this_thread::sleep_for(seconds(processTime));
+    {
+        lock_guard<mutex> lock(consoleMutex);
+        cout << "[ГОТОВО] Пакет #" << packet.id << " обработан за "
+            << processTime << " сек" << endl;
+    }
+    totalActivePackets--;
+    packetsProcessed++;
+    serverSem.release();  
+}
+void monitorLoad() {
+    while (systemRunning) {
+        this_thread::sleep_for(seconds(3));
+        int currentServers = activeServers.load();
+        int activePackets = totalActivePackets.load();
+        double load = (double)activePackets / (currentServers * 2);
+        {
+            lock_guard<mutex> lock(consoleMutex);
+            cout << "\n [МОНИТОРИНГ] Загрузка: " << (load * 100) << "%"
+                << " | Активных пакетов: " << activePackets
+                << " | Серверов: " << currentServers
+                << " | Обработано: " << packetsProcessed.load()
+                << " / " << packetsSent.load() << endl;
+        }
+        if (load > LOAD_THRESHOLD && currentServers < MAX_SERVERS) {
+            {
+                lock_guard<mutex> lock(consoleMutex);
+                cout << " [АДАПТАЦИЯ] Загрузка превысила 80%!" << endl;
+                cout << " Включаем дополнительный сервер..." << endl;
+            }
+
+            activeServers++;
+            serverSem.release();  
+        }
+        if (load > 1.2 && !emergencyMode) {
+            emergencyMode = true;
+            {
+                lock_guard<mutex> lock(consoleMutex);
+                cout << " [АВАРИЯ] Критическая перегрузка системы!" << endl;
+                cout << " Низкоприоритетные данные будут отброшены" << endl;
+            }
+            lock_guard<mutex> lock(queueMutex);
+            priority_queue<DataPacket> newQueue;
+            int dropped = 0;
+            while (!packetQueue.empty()) {
+                DataPacket p = packetQueue.top();
+                packetQueue.pop();
+                if (p.isCritical || p.priority <= 3) {
+                    newQueue.push(p);
+                }
+                else {
+                    dropped++;
+                    lock_guard<mutex> lock(consoleMutex);
+                    cout << " Отброшен пакет #" << p.id
+                        << " (приоритет: " << p.priority << ")" << endl;
+                }
+            }
+            packetQueue = newQueue;
+            {
+                lock_guard<mutex> lock(consoleMutex);
+                cout << "Отброшено пакетов: " << dropped << endl;
+            }
+            this_thread::sleep_for(seconds(2));
+            emergencyMode = false;
+            {
+                lock_guard<mutex> lock(consoleMutex);
+                cout << " Аварийный режим завершен" << endl;
+            }
+        }
+    }
+}
+void distributePackets() {
+    while (systemRunning) {
+        DataPacket packet;
+        bool hasPacket = false;
+        {
+            lock_guard<mutex> lock(queueMutex);
+            if (!packetQueue.empty()) {
+                packet = packetQueue.top();
+                packetQueue.pop();
+                hasPacket = true;
+            }
+        }
+        if (hasPacket) {
+            thread t(processPacket, packet);
+            t.detach();
+        }
+        else {
+            this_thread::sleep_for(milliseconds(100));
+        }
+    }
+}
+void monitoringStation(int stationId) {
+    for (int i = 0; i < PACKETS_PER_STATION; i++) {
+        if (!systemRunning) break;
+        DataPacket packet;
+        packet.id = packetCounter++;
+        packet.priority = priorityDist(gen);
+        packet.size = sizeDist(gen);
+        packet.stationId = stationId;
+        packet.isCritical = (criticalChance(gen) <= 2) || (packet.priority <= 2);
+        {
+            lock_guard<mutex> lock(queueMutex);
+            packetQueue.push(packet);
+        }
+        packetsSent++;
+        {
+            lock_guard<mutex> lock(consoleMutex);
+            cout << " [СТАНЦИЯ " << stationId << "] Отправлен пакет #" << packet.id
+                << " | приоритет: " << packet.priority
+                << (packet.isCritical ? " [КРИТИЧЕСКИЙ]" : "")
+                << " | размер: " << packet.size << " усл.ед." << endl;
+        }
+        int delay = 500; 
+        if (stationId == 5) {
+            delay = 200;
+        }
+        this_thread::sleep_for(milliseconds(delay));
+    }
+    {
+        lock_guard<mutex> lock(consoleMutex);
+        cout << " [СТАНЦИЯ " << stationId << "] Завершила отправку пакетов" << endl;
+    }
+}
+int main() {
+    setlocale(LC_ALL, "Russian");
+    cout << "АДАПТИВНАЯ СИСТЕМА МОНИТОРИНГА ЭНЕРГОСЕТИ" << endl;
+    cout << "Пакетов от каждой станции: " << PACKETS_PER_STATION << endl;
+    cout << "Всего пакетов: " << TOTAL_STATIONS * PACKETS_PER_STATION << endl;
+    cout << "Начальное количество серверов: " << INITIAL_SERVERS << endl;
+    cout << "Максимум серверов: " << MAX_SERVERS << endl;
+    cout << "Порог загрузки для добавления сервера: 80%" << endl;
+    cout << "Станция #5 создает повышенную нагрузку" << endl;
+    thread monitor(monitorLoad);
+    vector<thread> distributors;
+    for (int i = 0; i < 3; i++) {
+        distributors.emplace_back(distributePackets);
+    }
+    vector<thread> stations;
+    for (int i = 1; i <= TOTAL_STATIONS; i++) {
+        stations.emplace_back(monitoringStation, i);
+        this_thread::sleep_for(milliseconds(20));
+    }
+    for (auto& t : stations) {
+        t.join();
+    }
+
+    cout << "\n Все станции завершили отправку пакетов" << endl;
+    cout << " Ожидаем обработки оставшихся пакетов..." << endl;
+    while (packetsProcessed.load() < packetsSent.load()) {
+        this_thread::sleep_for(seconds(1));
+        {
+            lock_guard<mutex> lock(consoleMutex);
+            cout << "Обработано: " << packetsProcessed.load()
+                << " / " << packetsSent.load() << endl;
+        }
+    }
+
+    systemRunning = false;
+    this_thread::sleep_for(seconds(2));
+    for (auto& t : distributors) {
+        if (t.joinable()) t.detach();
+    }
+    if (monitor.joinable()) monitor.detach();
+    cout << "СИСТЕМА ОСТАНОВЛЕНА" << endl;
+    cout << "Всего отправлено пакетов: " << packetsSent.load() << endl;
+    cout << "Всего обработано пакетов: " << packetsProcessed.load() << endl;
+    return 0;
+}
---
+++ Лаба2_зад1_прв/Лаба2_зад1_прв/Лаба2_зад1_прв.cpp
@@ -0,0 +1,193 @@
+﻿#include <iostream>
+#include <thread>
+#include <vector>
+#include <queue>
+#include <mutex>
+#include <semaphore>
+#include <chrono>
+#include <random>
+#include <atomic>
+
+using namespace std;
+using namespace chrono;
+
+struct Task {
+    int id;
+    int priority;
+    int duration;
+    bool isCritical;
+};
+
+struct TaskComparator {
+    bool operator()(const Task& a, const Task& b) {
+        if (a.isCritical != b.isCritical) {
+            return b.isCritical;
+        }
+        return a.priority > b.priority;
+    }
+};
+
+const int TOTAL_PROCESSORS = 4;
+atomic<int> activeProcessors(TOTAL_PROCESSORS);
+atomic<int> totalActiveTasks(0);
+atomic<int> taskCounter(0);
+atomic<bool> systemRunning(true);
+
+counting_semaphore<TOTAL_PROCESSORS> processorSem(TOTAL_PROCESSORS);
+mutex consoleMutex;
+mutex queueMutex;
+priority_queue<Task, vector<Task>, TaskComparator> taskQueue;
+
+vector<thread> workerThreads;
+mutex workersMutex;
+
+random_device rd;
+mt19937 gen(rd());
+uniform_int_distribution<> durationDist(1, 5);
+uniform_int_distribution<> priorityDist(1, 10);
+
+void processTask(Task task) {
+    processorSem.acquire();
+    totalActiveTasks++;
+    {
+        lock_guard<mutex> lock(consoleMutex);
+        cout << "[ПРОЦЕССОР] обрабатывает задачу #" << task.id
+            << " | приоритет: " << task.priority
+            << (task.isCritical ? " | КРИТИЧЕСКАЯ" : "")
+            << " | время: " << task.duration << " сек" << endl;
+    }
+    this_thread::sleep_for(seconds(task.duration));
+    if (totalActiveTasks.load() > activeProcessors.load() * 2) {
+        lock_guard<mutex> lock(queueMutex);
+        if (task.duration > 1) {
+            Task subtask1 = { taskCounter++, task.priority, task.duration / 2, task.isCritical };
+            Task subtask2 = { taskCounter++, task.priority, task.duration - task.duration / 2, task.isCritical };
+            taskQueue.push(subtask1);
+            taskQueue.push(subtask2);
+            {
+                lock_guard<mutex> lock(consoleMutex);
+                cout << "[РАЗДЕЛЕНИЕ] Задача #" << task.id
+                    << " разделена на #" << subtask1.id
+                    << " и #" << subtask2.id << endl;
+            }
+        }
+    }
+    {
+        lock_guard<mutex> lock(consoleMutex);
+        cout << "[ГОТОВО] Задача #" << task.id << " завершена" << endl;
+    }
+    totalActiveTasks--;
+    processorSem.release();
+}
+
+void distributeTasks() {
+    while (systemRunning) {
+        Task task;
+        bool hasTask = false;
+        {
+            lock_guard<mutex> lock(queueMutex);
+            if (!taskQueue.empty()) {
+                task = taskQueue.top();
+                taskQueue.pop();
+                hasTask = true;
+            }
+        }
+        if (hasTask) {
+            lock_guard<mutex> lock(workersMutex);
+            workerThreads.emplace_back(processTask, task);
+        }
+        else {
+            this_thread::sleep_for(milliseconds(100));
+        }
+    }
+}
+
+void monitorSystem() {
+    this_thread::sleep_for(seconds(5));
+    if (systemRunning) {
+        {
+            lock_guard<mutex> lock(consoleMutex);
+            cout << "\n!!! [АВАРИЯ] Квантовый процессор #2 вышел из строя !!!" << endl;
+            cout << "[ПЕРЕРАСПРЕДЕЛЕНИЕ] Задачи переносятся на процессоры #1, #3, #4" << endl;
+        }
+        if (!processorSem.try_acquire()) {
+            processorSem.acquire();
+        }
+        activeProcessors--;
+        {
+            lock_guard<mutex> lock(consoleMutex);
+            cout << "Активных процессоров: " << activeProcessors.load() << endl;
+            cout << "Задачи в очереди будут обработаны оставшимися процессорами\n" << endl;
+        }
+    }
+    while (systemRunning) {
+        this_thread::sleep_for(seconds(3));
+        {
+            lock_guard<mutex> lock(consoleMutex);
+            cout << "\n[СТАТУС] Активных задач: " << totalActiveTasks.load()
+                << " | В очереди: " << taskQueue.size()
+                << " | Процессоров: " << activeProcessors.load() << endl;
+        }
+    }
+}
+
+void taskGenerator() {
+    int taskId = 1;
+    for (int i = 0; i < 10; i++) {
+        bool isCritical = (i % 3 == 0);
+        int priority = isCritical ? 1 : priorityDist(gen);
+        int duration = durationDist(gen);
+        Task task = { taskId++, priority, duration, isCritical };
+        {
+            lock_guard<mutex> lock(queueMutex);
+            taskQueue.push(task);
+        }
+        {
+            lock_guard<mutex> lock(consoleMutex);
+            cout << "[ДОБАВЛЕНО] Задача #" << task.id
+                << (isCritical ? " [КРИТИЧЕСКАЯ]" : "")
+                << " | приоритет: " << priority
+                << " | время: " << duration << " сек" << endl;
+        }
+        this_thread::sleep_for(milliseconds(500));
+    }
+    {
+        lock_guard<mutex> lock(consoleMutex);
+        cout << "\nВсе 10 задач добавлены в очередь\n" << endl;
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    cout << "КВАНТОВЫЙ СИМУЛЯТОР" << endl;
+    cout << "Доступно процессоров: " << TOTAL_PROCESSORS << endl;
+    cout << "Критические задачи имеют наивысший приоритет" << endl;
+    cout << "При перегрузке задачи автоматически разделяются" << endl;
+    cout << "Через 5 секунд произойдет отказ одного процессора\n" << endl;
+    thread generator(taskGenerator);
+    thread monitor(monitorSystem);
+    vector<thread> distributors;
+    for (int i = 0; i < 3; i++) {
+        distributors.emplace_back(distributeTasks);
+    }
+    generator.join();
+    while (totalActiveTasks.load() > 0 || !taskQueue.empty()) {
+        this_thread::sleep_for(seconds(1));
+        cout << "Ожидание завершения... Активных задач: " << totalActiveTasks.load()
+            << ", в очереди: " << taskQueue.size() << endl;
+    }
+    systemRunning = false;
+    for (auto& t : distributors) {
+        if (t.joinable()) t.join();
+    }
+    if (monitor.joinable()) monitor.join();
+    {
+        lock_guard<mutex> lock(workersMutex);
+        for (auto& t : workerThreads) {
+            if (t.joinable()) t.join();
+        }
+    }
+    cout << "\nВСЕ ЗАДАЧИ ВЫПОЛНЕНЫ!" << endl;
+    cout << "Всего создано задач: " << taskCounter.load() << endl;
+    return 0;
+}
